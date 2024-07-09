1da79fe017e4dfd640a43e83be544c46025fc037
unofficialabj <unofficial.abj@gmail.com>
2024-07-09 17:01:53 +0545

    learning the difference between map, filter and find

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,52 @@
 const About = () => {
-  return (
-    <div>
-      <h1>This is About</h1>
-    </div>
-  );
+  const detail = [
+    {
+      name: "John Doe",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      name: "Jane Smith",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      name: "Alice Johnson",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      name: "Bob Brown",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      name: "Charlie Davis",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      name: "Eve White",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+    {
+      name: "Jane Smith",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+  ];
+
+  const mapResult = detail.map((item) => item);
+
+  console.log(mapResult)
+
+  const findResult = detail.find((item) => item.name === "Jane Smith");
+  const filterResult = detail.filter((item) => item.name === "Jane Smith");
+  console.log(findResult, "find");
+  console.log(filterResult, "filter");
+
+  return <div></div>;
 };
 
 export default About;
